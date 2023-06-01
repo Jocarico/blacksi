449466c47dbeab0ec830715d04387b143e909ee2
Jonatan Rico <jonatancalebrico@gmail.com>
2023-05-31 21:05:01 -0600

    backend with mongodb connection made

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,45 +1,75 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-
+const express = require("express");
 const app = express();
-const port = 3000;
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const env = require("dotenv");
+const Admin = require('./models/adminModel');
+const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
 
-// Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to the login page');
+env.config({
+    path: "./config.env",
 });
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+app.use(cors());
+app.use(bodyparser.json({limit: "100mb"}));
 
-  // Perform validation or authentication checks here
-  // For simplicity, let's assume username is 'admin' and password is 'password'
-  if (username === 'admin' && password === 'password') {
-    req.session.loggedIn = true;
-    res.redirect('/dashboard');
-  } else {
-    res.send('Invalid username or password');
-  }
+// Establish MongoDB connection
+mongoose.connect('mongodb+srv://simpleman2:simpleman2@cluster0.rqx2w1f.mongodb.net/ProyectoUTCH?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
 
-app.get('/dashboard', (req, res) => {
-  if (req.session.loggedIn) {
-    res.send('Welcome to the dashboard');
-  } else {
-    res.redirect('/');
-  }
+app.all("*", (res, req, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
 });
 
-// Start the server
+app.post('/login', async function (req, res) {
+    const {email, password} = req.body;
+
+    try {
+        const admin = await Admin.findOne({email: email, password: password});
+        if (admin) {
+            res.send("Login successful!");
+        } else {
+            res.status(401).send("Invalid login credentials");
+        }
+    } catch (error) {
+        res.status(500).send("An error occurred while logging in");
+    }
+});
+
+app.post('/register', async function (req, res) {
+    const {email, password} = req.body;
+
+    try {
+        const existingAdmin = await Admin.findOne({email: email});
+        if (existingAdmin) {
+            return res.status(409).send("Email already registered");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newAdmin = new Admin({
+            email: email,
+            password: hashedPassword
+        });
+
+        await newAdmin.save();
+
+        res.send("Registration successful!");
+    } catch (error) {
+        res.status(500).send("An error occurred while registering");
+    }
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+    console.log(`App is running on port: ${port}`);
 });
