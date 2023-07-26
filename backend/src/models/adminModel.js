50909c4b9c05e1ac65635081f2d5c42a63f2e9bd
Jonatan Rico <jonatancalebrico@gmail.com>
2023-07-25 18:15:38 -0600

    CRUD created for control panel

--- backend/src/models/adminModel.js
+++ backend/src/models/adminModel.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
+  lastName: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
--- backend/src/server.js
+++ backend/src/server.js
@@ -43,6 +43,20 @@ function generateToken(payload) {
   const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
   return token;
 }
+//Function to obtain just 1 driver with the phoneNumber
+const getDriver = async(req,res,next) =>{
+  let driver;
+  try {
+    driver = await Admin.findById(req.params.id);
+    if (driver == null) {
+      return res.status(404).json( {message: 'Cannot find Driver with that ID'})
+    }
+  } catch (err) {
+    return res.status(500).json({message: err.message});
+  }
+  res.driver = driver;
+  next();
+}
 
 // Establish MongoDB connection
 mongoose.connect(database_connection, {
@@ -78,41 +92,91 @@ app.post('/login', async (req, res) => {
     } catch (error) {
       res.status(500).send("An error occurred while logging in");
     }
-  });
-  app.post('/register', async function (req, res) {
-    const { email, password } = req.body;
-  
+});
+app.post('/add', async (req, res) => {
+  const { lastName, firstName, phoneNumber, address, email, password } = req.body;
+
+  try {
+    const existingAdmin = await Admin.findOne({ email: email });
+    if (existingAdmin) {
+      return res.status(409).send("Email already registered");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newAdmin = new Admin({
+      lastName: lastName,
+      firstName: firstName,
+      phoneNumber: phoneNumber,
+      address: address,
+      email: email,
+      password: hashedPassword
+    });
+
     try {
-      const existingAdmin = await Admin.findOne({ email: email });
-      if (existingAdmin) {
-        return res.status(409).send("Email already registered");
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const newAdmin = new Admin({
-        email: email,
-        password: hashedPassword
-      });
-  
-      try {
-        await newAdmin.save();
-        res.send("Registration successful!");
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred while registering");
-      }
+      await newAdmin.save();
+      res.status(201).send("Registration successful!");
     } catch (error) {
-      console.log(error);
+      console.error(error);
       res.status(500).send("An error occurred while registering");
     }
-  });
-  
-  app.get('/deliveryInfo', async function (req, res) {
-    const datatn = await tiendanubeData(req,res);
- 
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred while registering");
+  }
+});
+app.get('/driverList', async (req,res) => {
+  try {
+    const listOfDrivers = await Admin.find();
+    res.json(listOfDrivers)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+});
+app.patch('/updateDriverInfo/:id', getDriver, async (req,res) => {
+  if (req.body.lastName != null) {
+    res.driver.lastName = req.body.lastName;
+  }
+  if (req.body.firstName != null) {
+    res.driver.firstName = req.phoneNumber.firstName;
+  }if (req.body.phoneNumber != null) {
+    res.driver.phoneNumber = req.body.phoneNumber;
+  }
+  if (req.body.address != null) {
+    res.driver.address = req.body.address;
+  }
+  if (req.body.email != null) {
+    res.driver.email = req.body.email;
+  }
+  if (req.body.password != null) {
+    const newPassword = await bcrypt.hash(req.body.password, 10);
+    res.driver.password = newPassword
+  }
+  try {
+    const updatedDriver = await res.driver.save();
+    res.json(updatedDriver)
+  } catch (err) {
+    res.status(400).json({message: err.message })
+  }
+});
+app.delete('/deleteDriver/:id', getDriver, async (req,res) => {
+  try {
+    await res.driver.remove();
+    res.json({message: 'Driver deleted'})
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
+});
+app.get('/getDriver/:id', getDriver, (req,res) => {
+  res.send(res.driver)
+})
 
 
-  })
+app.get('/deliveryInfo', async function (req, res) {
+  const datatn = await tiendanubeData(req,res);
+
+
+
+})
     
 
 const port = process.env.PORT || 3000;
